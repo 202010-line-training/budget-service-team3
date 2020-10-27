29c81c0183e0503001f35f5b249a4b9bdc937a51
Joey Chen <jay.hatelove@gmail.com>
2020-10-27 21:49:39 +0800

    change firstDay value from another.start, and lastDay value from another.end

--- src/main/java/main/Period.java
+++ src/main/java/main/Period.java
@@ -23,8 +23,8 @@ public class Period {
 
     long getOverlappingDays(Budget budget) {
         Period another = new Period(budget.firstDay(), budget.lastDay());
-        LocalDate firstDay = budget.firstDay();
-        LocalDate lastDay = budget.lastDay();
+        LocalDate firstDay = another.start;
+        LocalDate lastDay = another.end;
         LocalDate overlappingStart = getStart().isAfter(firstDay)
                 ? getStart()
                 : firstDay;
