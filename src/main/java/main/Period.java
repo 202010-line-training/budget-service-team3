6cc78d24c2a3fbe13895aa8356cc45ab323f5407
Joey Chen <jay.hatelove@gmail.com>
2020-10-27 22:01:17 +0800

    clean up Period class

--- src/main/java/main/Period.java
+++ src/main/java/main/Period.java
@@ -13,20 +13,12 @@ public class Period {
         this.end = end;
     }
 
-    public LocalDate getStart() {
-        return start;
-    }
-
-    public LocalDate getEnd() {
-        return end;
-    }
-
-    long getOverlappingDays(Period another) {
-        LocalDate overlappingStart = getStart().isAfter(another.start)
-                ? getStart()
+    public long getOverlappingDays(Period another) {
+        LocalDate overlappingStart = start.isAfter(another.start)
+                ? start
                 : another.start;
-        LocalDate overlappingEnd = getEnd().isBefore(another.end)
-                ? getEnd()
+        LocalDate overlappingEnd = end.isBefore(another.end)
+                ? end
                 : another.end;
         return DAYS.between(overlappingStart, overlappingEnd) + 1;
     }
