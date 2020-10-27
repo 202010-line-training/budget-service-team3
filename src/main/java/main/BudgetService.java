c2974b574304b79066edc5925316b4009ba87862
Joey Chen <jay.hatelove@gmail.com>
2020-10-27 20:10:28 +0800

    change intervalDays definition

--- src/main/java/main/BudgetService.java
+++ src/main/java/main/BudgetService.java
@@ -23,11 +23,8 @@ public class BudgetService {
         budgetMap = allBudgets.stream()
                 .collect(Collectors.toMap(budget -> budget.getYearMonth(), budget -> budget));
         if (YearMonth.from(start).equals(YearMonth.from(end))) {
-//            if (start.getDayOfMonth() == end.getDayOfMonth()) {
-//                return getSingleDayBudget(start);
-//            }
-            final int intervalDays = end.getDayOfMonth() - start.getDayOfMonth();
-            return getSingleDayBudget(start) * (intervalDays + 1);
+            final int intervalDays = end.getDayOfMonth() - start.getDayOfMonth() + 1;
+            return getSingleDayBudget(start) * intervalDays;
         }
 
         double ans = getFirstMonthBudget(start) + getLastMonthBudget(end);
